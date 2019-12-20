7abcae12600f5702744ae54edb574ab039a128c0
Levi Schneider <digitaldenizen1848@gmail.com>
2019-12-20 13:50:56 -0500

    Adding latest physics changes

--- Player.gd
+++ Player.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
 const MOVE_SPEED = 300
+signal shoot(bullet, direction, location)
 
 onready var raycast = $RayCast2D
 onready var fire_ball = $"Fire-Ball"
+var FireBall = preload("res://Fire-Ball.tscn")
 
 func _ready():
 	yield(get_tree(), "idle_frame")
@@ -22,8 +24,19 @@ func _physics_process(delta):
 	move_vec = move_vec.normalized()
 	move_and_collide(move_vec * MOVE_SPEED * delta)
 	
-	if Input.is_action_just_pressed("Attack"):
-		var coll = fire_ball.launch(1)
-	
 func kill():
 	get_tree().reload_current_scene()
+	
+func _input(event):
+    if event is InputEventMouseButton:
+        if event.button_index == BUTTON_LEFT and event.pressed:
+            emit_signal("shoot", FireBall, rotation, position)
+
+func _process(delta):
+    look_at(get_global_mouse_position())
+	
+func _on_Player_shoot(fireball, direction, location):
+    var b = FireBall.instance()
+    add_child(b)
+    b.position = location
+    b.velocity = b.velocity.rotated(direction)
