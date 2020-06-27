91327452ad82e92661f45a1baa550924a2577c47
Levi <DigitalDenizen1848@gmail.com>
2020-06-27 12:21:36 -0400

    Merging master into mob-spawn branch

--- Characters/Combat/Melee.gd
+++ Characters/Combat/Melee.gd
@@ -25,7 +25,7 @@ func _body_entered(body):
 			body.hurt(25)
 			queue_free()
 	elif attacker == "Zombie":
-		if !body.name.begins_with("Zombie"):
+		if !body.name.begins_with("@Zombie"):
 			body.hurt(10)
 			queue_free()
 		
