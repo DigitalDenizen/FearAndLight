1de42d58639e825ab68a9a5d8d9e3d269be3b8e1
Xacarob <vidwan.rag@gmail.com>
2020-10-21 17:31:03 -0400

    intro finalized

--- UI/intro.gd
+++ UI/intro.gd
@@ -1,14 +1,69 @@
 extends Node2D
 
-func _ready():
-	$dog_talk.visible = false
-	_enter()
+var dog_frame = 1
+var dialog_text = 1
 
-func _enter():	
-	$dog_enter.play("enter")	
+func _ready():
+	$spirit_dog/speech_bubble.visible = false	
+
+func _dog_enter():	
+	$spirit_dog.play("enter")	
+		
+func _dog_talk():	
+	$spirit_dog.play("talk")
+	$spirit_dog/speech_bubble.visible = true
+	_speech1()
+
+func _on_Button_pressed():
+	if dog_frame == 1:
+		_dog_enter()
+		print ("first click")
+		dog_frame = 2
+	elif dog_frame == 2:
+		_dog_talk()
+		print("second click")
+		$Button.visible = false
+		
+
+func _speech1():
+	$spirit_dog/speech_bubble/Speech1.visible = true
+	$spirit_dog/speech_bubble/Speech2.visible = false
+	$spirit_dog/speech_bubble/Speech3.visible = false
+	$spirit_dog/speech_bubble/Speech4.visible = false
+	dialog_text = 2
 	
-func _speech():	
-	$dog_enter.visible = false
-	$dog_talk.visible = true
-	$dog_talk.play("talk")
+func _speech2():
+	$spirit_dog/speech_bubble/Speech1.visible = false
+	$spirit_dog/speech_bubble/Speech2.visible = true
+	$spirit_dog/speech_bubble/Speech3.visible = false
+	$spirit_dog/speech_bubble/Speech4.visible = false
+	dialog_text = 3
 	
+func _speech3():
+	$spirit_dog/speech_bubble/Speech1.visible = false
+	$spirit_dog/speech_bubble/Speech2.visible = false
+	$spirit_dog/speech_bubble/Speech3.visible = true
+	$spirit_dog/speech_bubble/Speech4.visible = false
+	dialog_text = 4
+
+func _speech4():
+	$spirit_dog/speech_bubble/Speech1.visible = false
+	$spirit_dog/speech_bubble/Speech2.visible = false
+	$spirit_dog/speech_bubble/Speech3.visible = false
+	$spirit_dog/speech_bubble/Speech4.visible = true
+	dialog_text = 1
+
+
+func _on_Text_Button_pressed():
+	if dialog_text == 1:
+		_speech1()
+	elif dialog_text == 2:
+		_speech2()		
+	elif dialog_text == 3:
+		_speech3()
+	elif dialog_text == 4:
+		_speech4()
+
+
+func _on_skip_button_pressed():
+	get_tree().change_scene("res://UI/title-screen.tscn")
--- click_area.gd
+++ click_area.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
-func _on_Click_area_input_event(viewport, event, shape_idx):
+
+func _on_click_area_input_event(viewport, event, shape_idx):
 	if (event.type == InputEvent.MOUSEBUTTON && event.is_pressed and event.button_index == BUTTON_LEFT):
 		print("Clicked")
