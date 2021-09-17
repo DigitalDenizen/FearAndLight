4b22d706b8108ecacbd7745052fa3ed8c3cddd96
Fred Kumih <kumih54@gmail.com>
2021-09-16 20:13:06 -0400

    UI layout construction

--- UI/Battle/Battle_Panel.gd
+++ UI/Battle/Battle_Panel.gd
@@ -1,14 +1,7 @@
 extends PanelContainer
+
 signal battleStart()
-
-func _ready():
-	
-	pass # Replace with function body.
-
-func _process(delta):
-	pass
 
 func _on_Start_Battle_toggled(button_pressed):
 	if button_pressed == true:
 		emit_signal("battleStart")
-		
