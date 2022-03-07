5af2449ad9fe9f7665dfb38dd20a9296f71899a9
Xacarob <vidwan.rag@gmail.com>
2022-03-07 18:07:45 -0500

    Placing structures
    You can now place the Mud Hut and Large Mud Wall

--- Shared/EventBus.gd
+++ Shared/EventBus.gd
@@ -2,3 +2,10 @@ extends Node
 
 
 signal toggle_inventory(msg)
+
+signal build_menu_opened() #This signal is emitted whenever the build menu is opened
+signal build_menu_closed() #This signal is used to capture the event when the build menu is closed, either by the user or a proces
+signal close_build_menu() #This signal is used to close the build menu
+signal placing_mud_hut() #Emitted when user clicks on the Mud Hut's Place It button
+signal placing_large_wall() #Emitted when user clicks on the Large Wall's Place It button
+
