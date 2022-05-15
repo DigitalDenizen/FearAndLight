81a03829ed82d0a2e55252355429995bcd72f987
Jeremy Fleischer <jeremy@jeremyfleicher.com>
2022-05-15 00:31:04 -0400

    New Images

--- UI/Cursors/Cursor.gd
+++ UI/Cursors/Cursor.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 onready var emptyCursor = preload("res://Images/UI/Cursors/ui-cursor-empty.png")
 onready var arrowCursor = preload("res://Images/UI/Cursors/ui-cursor-arrow.png")
@@ -14,24 +14,19 @@ func _ready():
 	
 func _process(delta):
 	self.position = self.get_global_mouse_position()
-	
+
 #	Potential usage
 #	Input.set_custom_mouse_cursor(pointerCursor, Input.CURSOR_POINTING_HAND, Vector2())
 
 func _on_Cursor_body_entered(body):
-	var cursor = Util.get_main_node().get_node("YSort/Player/Camera2D/Overlay/GridContainer/Cursor")
 	if body.is_in_group("Objects"):
 		self.Icon.texture = targetCursor
-		cursor.visible = false
+		print("I SEE OBJECTS")
 	if body.is_in_group("Baddies"):
 		self.Icon.texture = swordCursor
-		cursor.visible = false
 	if body.is_in_group("Structures"):
 		self.Icon.texture = hammerCursor
-		cursor.visible = false
 
 func _on_Cursor_body_exited(body):
-	var cursor = Util.get_main_node().get_node("YSort/Player/Camera2D/Overlay/GridContainer/Cursor")
 	if body.is_in_group("Objects") || body.is_in_group("Baddies") || body.is_in_group("Structures"):
 		self.Icon.texture = arrowCursor
-		cursor.visible = true
