4a4a76b733163e445408ceaff25dc72d78d89914
Jeremy Fleischer <jeremy@jeremyfleicher.com>
2022-05-20 15:59:58 -0400

    Initial Commit

---
+++ World/Door.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+export(PackedScene) var target_scene
+
+func _ready():
+	pass
+	
+func _input(event):
+	if event.is_action_pressed("shoot") || event.is_action_pressed("melee"):
+		if !target_scene: # is null
+			print ("no scene is this door")
+			return
+		if get_overlapping_bodies().size() > 0:
+			next_level()
+			
+func next_level():
+	var ERR = get_tree().change_scene_to(target_scene)
