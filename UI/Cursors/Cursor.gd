b3576eaae9cf6f09cf6c109bb555e48dfa6df347
Jeremy Fleischer <jeremy@jeremyfleicher.com>
2022-05-23 22:00:27 -0400

    Removed Faulty Export Variables

--- UI/Cursors/Cursor.gd
+++ UI/Cursors/Cursor.gd
@@ -1,18 +1,14 @@
 extends Area2D
 
-export(Texture) var empty_cursor = null
-export(Texture) var arrow_cursor = null
-export(Texture) var target_cursor = null
-export(Texture) var pointer_cursor = null
-export(Texture) var sword_cursor = null
-export(Texture) var ibeam_cursor = null
-export(Texture) var hand_open_cursor = null
-export(Texture) var hand_grab_cursor = null
-
-export(Vector2) var base_window_size = Vector2.ZERO
-export(Vector2) var base_cursor_size = Vector2.ZERO
-
+onready var arrowCursor = preload("res://Images/UI/Cursors/ui-cursor-arrow.png")
+onready var targetCursor = preload("res://Images/UI/Cursors/ui-cursor-target.png")
+onready var pointerCursor = preload("res://Images/UI/Cursors/ui-cursor-pointer.png")
+onready var swordCursor = preload("res://Images/UI/Cursors/ui-cursor-sword.png")
+onready var ibeamCursor = preload("res://Images/UI/Cursors/ui-cursor-ibeam.png")
+onready var handOpenCursor = preload("res://Images/UI/Cursors/ui-cursor-handopen.png")
+onready var handGrabCursor = preload("res://Images/UI/Cursors/ui-cursor-handgrab.png")
 onready var emptyCursor = preload("res://Images/UI/Cursors/ui-cursor-empty.png")
+
 onready var enemyIcon = preload("res://Images/UI/Cursors/ui-cursor-icon-enemy.png")
 onready var defenseIcon = preload("res://Images/UI/Cursors/ui-cursor-icon-defense.png")
 onready var objectIcon = preload("res://Images/UI/Cursors/ui-cursor-icon-object.png")
@@ -22,19 +18,20 @@ onready var mushroomIcon = preload("res://Images/UI/Cursors/ui-cursor-icon-mushr
 onready var doorIcon = preload("res://Images/UI/Cursors/ui-cursor-icon-door.png")
 onready var heartIcon = preload("res://Images/UI/Cursors/ui-cursor-icon-heart.png")
 onready var bossIcon = preload("res://Images/UI/Cursors/ui-cursor-icon-boss.png")
+
 onready var Icon = self.get_node("Sprite")
 
 func _ready():
 	update_cursor()
 	get_tree().connect("screen_resized", self, "update_cursor")
-	Icon.hide()
+	print("huh")
 	
 func _physics_process(delta):
 	self.position = self.get_global_mouse_position()
 	
 func update_cursor():
 	var current_window_size = OS.window_size
-	var scale_multiple = min(floor(current_window_size.x / base_window_size.x), floor(current_window_size.y / base_window_size.y))
+	var scale_multiple = min(floor(current_window_size.x / 640), floor(current_window_size.y / 360))
 	
 	# Get Images
 	var texture_arrow = ImageTexture.new()
@@ -44,18 +41,18 @@ func update_cursor():
 	var texture_hand_open = ImageTexture.new()
 	
 	# Get Texture Data
-	var arrow = arrow_cursor.get_data()
-	var pointer = pointer_cursor.get_data()
-	var ibeam = ibeam_cursor.get_data()
-	var sword = sword_cursor.get_data()
-	var hand_open = hand_open_cursor.get_data()
+	var arrow = arrowCursor.get_data()
+	var pointer = pointerCursor.get_data()
+	var ibeam = ibeamCursor.get_data()
+	var sword = swordCursor.get_data()
+	var hand_open = handOpenCursor.get_data()
 	
 	# Interpolate
-	arrow.resize(base_cursor_size.x * (scale_multiple), base_cursor_size.y * (scale_multiple ), Image.INTERPOLATE_NEAREST)
-	pointer.resize(base_cursor_size.x * (scale_multiple), base_cursor_size.y * (scale_multiple), Image.INTERPOLATE_NEAREST)
-	ibeam.resize(base_cursor_size.x * (scale_multiple), base_cursor_size.y * (scale_multiple), Image.INTERPOLATE_NEAREST)
-	sword.resize(base_cursor_size.x * (scale_multiple), base_cursor_size.y * (scale_multiple), Image.INTERPOLATE_NEAREST)
-	hand_open.resize(base_cursor_size.x * (scale_multiple), base_cursor_size.y * (scale_multiple), Image.INTERPOLATE_NEAREST)
+	arrow.resize(17 * (scale_multiple), 17 * (scale_multiple ), Image.INTERPOLATE_NEAREST)
+	pointer.resize(17 * (scale_multiple), 17 * (scale_multiple), Image.INTERPOLATE_NEAREST)
+	ibeam.resize(17 * (scale_multiple), 17 * (scale_multiple), Image.INTERPOLATE_NEAREST)
+	sword.resize(17 * (scale_multiple), 17 * (scale_multiple), Image.INTERPOLATE_NEAREST)
+	hand_open.resize(17 * (scale_multiple), 17 * (scale_multiple), Image.INTERPOLATE_NEAREST)
 	
 	# Create New Texture
 	texture_arrow.create_from_image(arrow)
@@ -73,28 +70,28 @@ func update_cursor():
 
 func arrow_cursor():
 	var current_window_size = OS.window_size
-	var scale_multiple = min(floor(current_window_size.x / base_window_size.x), floor(current_window_size.y / base_window_size.y))
+	var scale_multiple = min(floor(current_window_size.x / 640), floor(current_window_size.y / 360))
 	var texture_arrow = ImageTexture.new()
-	var arrow = arrow_cursor.get_data()
-	arrow.resize(base_cursor_size.x * (scale_multiple), base_cursor_size.y * (scale_multiple), Image.INTERPOLATE_NEAREST)
+	var arrow = arrowCursor.get_data()
+	arrow.resize(17 * (scale_multiple), 17 * (scale_multiple), Image.INTERPOLATE_NEAREST)
 	texture_arrow.create_from_image(arrow)
 	return texture_arrow
 	
 func pointer_cursor():
 	var current_window_size = OS.window_size
-	var scale_multiple = min(floor(current_window_size.x / base_window_size.x), floor(current_window_size.y / base_window_size.y))
+	var scale_multiple = min(floor(current_window_size.x / 640), floor(current_window_size.y / 360))
 	var texture_pointer = ImageTexture.new()
-	var pointer = pointer_cursor.get_data()
-	pointer.resize(base_cursor_size.x * (scale_multiple), base_cursor_size.y * (scale_multiple), Image.INTERPOLATE_NEAREST)
+	var pointer = pointerCursor.get_data()
+	pointer.resize(17 * (scale_multiple), 17 * (scale_multiple), Image.INTERPOLATE_NEAREST)
 	texture_pointer.create_from_image(pointer)
 	return texture_pointer
 	
 func target_cursor():
 	var current_window_size = OS.window_size
-	var scale_multiple = min(floor(current_window_size.x / base_window_size.x), floor(current_window_size.y / base_window_size.y))
+	var scale_multiple = min(floor(current_window_size.x / 640), floor(current_window_size.y / 360))
 	var texture_target = ImageTexture.new()
-	var target = target_cursor.get_data()
-	target.resize(base_cursor_size.x * (scale_multiple), base_cursor_size.y * (scale_multiple), Image.INTERPOLATE_NEAREST)
+	var target = targetCursor.get_data()
+	target.resize(17 * (scale_multiple), 17 * (scale_multiple), Image.INTERPOLATE_NEAREST)
 	texture_target.create_from_image(target)
 	return texture_target
 
@@ -107,7 +104,6 @@ func _on_Cursor_body_entered(body):
 		Icon.show()
 		self.Icon.texture = objectIcon
 		Input.set_custom_mouse_cursor(texture_target, Input.CURSOR_ARROW, Vector2(30, 30))
-#		      set_custom_mouse_cursor(Resource image, CursorShape shape=0, Vector2 hotspot=Vector2( 0, 0 ) )
 	if body.is_in_group("Baddies"):
 		Icon.show()
 		self.Icon.texture = enemyIcon
